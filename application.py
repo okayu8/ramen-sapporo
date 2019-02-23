78c199093509c4c4fb70cd79eb45e62fc82ca424
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-02-23 20:34:38 +0900

    recieve from API, and controll json

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, make_response, jsonify, render_template, request
 import io
 import http.client
 import urllib.request
@@ -20,74 +20,60 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-# headers = {
-#     # Request headers
-#     'Content-Type': 'application/octet-stream',
-#     'Prediction-key': '0f115d16825e47af8b61c98ede99124c',
-# }
 
-# params = urllib.parse.urlencode({
-#     # Request parameters
-#     'iterationId': 'dd2e1505-5010-4814-90fe-d411a9b912e6',
-# })
-
-# try:
-#     conn = http.client.HTTPSConnection(
-#         'southcentralus.api.cognitive.microsoft.com')
-#     f = open("original.jpg", "rb", buffering=0)
-#     conn.request("POST", "/customvision/v2.0/Prediction/a14c3964-9e4d-4cd8-b7fb-26b75796bd32/image?%s" %
-#                  params, f.readall(), headers)
-#     response = conn.getresponse()
-#     data = response.read()
-#     print(json.loads(data))
-#     conn.close()
-# except Exception as e:
-#     # print("[Errno {0}] {1}".format(e.errno, e.strerror))
-#     print(e)
+def getHitStore(stores):
+    print(stores)
+    return
 
 # '/' にget, postアクセスでhome()を通る
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    total = ''
+    img_file = ''
     # ボタンを押した時(post)の処理
-    if request.method == 'POST' and request.files['img_file']:
+    if request.method == 'POST':
+        if 'img_file' not in request.files:
+            return render_template('main.html')
+
         img_file = request.files['img_file']
 
-        headers = {
-            # Request headers
-            'Content-Type': 'multipart/form-data',
-            'Prediction-key': '0f115d16825e47af8b61c98ede99124c',
-        }
+        if img_file and allowed_file(img_file.filename):
+            headers = {
+                # Request headers
+                'Content-Type': 'multipart/form-data',
+                'Prediction-key': '0f115d16825e47af8b61c98ede99124c',
+            }
 
-        params = urllib.parse.urlencode({
-            # Request parameters
-            'iterationId': 'dd2e1505-5010-4814-90fe-d411a9b912e6',
-        })
-        data = ''
-        try:
-            conn = http.client.HTTPSConnection(
-                'southcentralus.api.cognitive.microsoft.com')
-            #f = open("original.jpg", "rb", buffering=0)
-            f = img_file.stream
-            # conn.request("POST", "/customvision/v2.0/Prediction/a14c3964-9e4d-4cd8-b7fb-26b75796bd32/image?%s" %
-            #              params, f.readall(), headers)
-            conn.request("POST", "/customvision/v2.0/Prediction/a14c3964-9e4d-4cd8-b7fb-26b75796bd32/image?%s" %
-                         params, f, headers)
-            response = conn.getresponse()
-            data = response.read()
-            conn.close()
-            print(json.loads(data.predictions))
-            #store = data.predictions[0]
-        except Exception as e:
-            # print("[Errno {0}] {1}".format(e.errno, e.strerror))
-            print(e)
+            params = urllib.parse.urlencode({
+                # Request parameters
+                'iterationId': 'dd2e1505-5010-4814-90fe-d411a9b912e6',
+            })
+            data = ''
+            try:
+                conn = http.client.HTTPSConnection(
+                    'southcentralus.api.cognitive.microsoft.com')
+                #f = open("original.jpg", "rb", buffering=0)
+                f = img_file.stream
+                # conn.request("POST", "/customvision/v2.0/Prediction/a14c3964-9e4d-4cd8-b7fb-26b75796bd32/image?%s" %
+                #              params, f.readall(), headers)
+                conn.request("POST", "/customvision/v2.0/Prediction/a14c3964-9e4d-4cd8-b7fb-26b75796bd32/image?%s" %
+                             params, f, headers)
+                response = conn.getresponse()
+                data = response.read()
+                conn.close()
+                stores = json.loads(data)
+                getHitStore(stores['predictions'])
+                print(stores)
+            except Exception as e:
+                # print("[Errno {0}] {1}".format(e.errno, e.strerror))
+                print(e)
 
-        print(json.dumps(request.form))
+            # total = count_word(text)
+            return render_template('main.html', sentence=data)
 
-        # total = count_word(text)
-        return render_template('main.html', total=total, sentence=data)
+        else:
+            return ''' <p>'png', 'jpg', 'gif' 以外のファイルはダメなんで</p> '''
 
-    return render_template('main.html', total=total)
+    return render_template('main.html')
 
 
 if __name__ == "__main__":
